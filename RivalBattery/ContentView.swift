77e2fd21e031e23236c90186c7ba4d089684ad80
felkr <ofelix@pm.me>
2022-05-15 16:04:40 +0200

    Initial Commit

---
+++ RivalBattery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RivalBattery
+//
+//  Created by Felix Kranich on 15.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RivalBattery/RivalBatteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RivalBatteryApp.swift
+//  RivalBattery
+//
+//  Created by Felix Kranich on 15.05.22.
+//
+
+import SwiftUI
+
+@main
+struct RivalBatteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
